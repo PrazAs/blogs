d58b96f7ba330dae6fd590afbfe3ac414196a9c0
root <root@ip-10-154-133-120.ec2.internal>
2016-08-26 13:34:27 +0000

    Plugins Addded

--- wp-content/themes/goodlife-wp-child/functions.php
+++ wp-content/themes/goodlife-wp-child/functions.php
@@ -1,7 +1,9 @@
 <?php
 
 if (function_exists('acf_add_options_page')) {
-    acf_add_options_page();
+    acf_add_options_page('Theme Settings');
+    acf_add_options_page('Tracking Settings');
+    acf_add_options_page('CTA Settings');
 }
 
 function output_scripts()
@@ -10,3 +12,18 @@ function output_scripts()
     the_field('facebook_tracking', 'option');
 }
 add_action('wp_footer', 'output_scripts');
+
+
+function cta_to_post($content)
+{
+    if (is_single()) {
+        $cta_url = get_field('cta_url');
+        $cta_img = get_field('cta_image');
+        if ($cta_img) {
+            $cta_output = '<a href="' . $cta_url . '"><img src="' . $cta_img . '"></a>';
+        }
+        $content .= $cta_output;
+    }
+    return $content;
+}
+add_filter('the_content', 'cta_to_post');
